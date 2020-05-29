e92e0fe3713482bb77178160063bd94fc34cce3f
Nishi <ngudai999@gmail.com>
2020-05-29 16:58:02 +0700

    add search books

--- Router/api/book.router.js
+++ Router/api/book.router.js
@@ -4,7 +4,16 @@ const router = express.Router();
 const Book = require('../../models/book.model');
 
 router.get('/', async function(req, res) {
-  var books = await Book.find();
+  let books = await Book.find();
+  res.json(books);
+});
+
+router.get('/search', async function(req, res) {
+  let q = req.query.q;
+  let books = await Book.find();
+  books = books.filter((item) => {
+	return item.title.toLowerCase().indexOf(q.toLowerCase()) !== -1; 
+  });
   res.json(books);
 });
 
