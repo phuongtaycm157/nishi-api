95b730297fcbcc3c4b99733b6d4687e53ed016a2
Nishi <ngudai999@gmail.com>
2020-05-29 16:27:54 +0700

    add cors module

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ require('dotenv').config()
 
 const express = require('express');
 const mongoose = require('mongoose');
+var cors = require('cors');
 
 const apiBook = require('./Router/api/book.router');
 
 const app = express();
 const port = process.env.PORT || 8008;
 
+app.use(cors())
+
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+
+app.use('/', (req, res) => res.send('Hello'))
 
 app.use('/api/books', apiBook);
 
